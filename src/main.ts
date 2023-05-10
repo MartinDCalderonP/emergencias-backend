5769d9bda44b3612e85e8be7b95cc53b2c1d796b
Martín <martincalderon92@outlook.com>
2023-05-10 18:39:15 -0300

    Swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Contacts API')
+    .setDescription('Contacts API for the Emergencias challenge')
+    .setVersion('1.0')
+    .addTag('contacts')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document, {
+    explorer: true,
+    swaggerOptions: {
+      filter: true,
+      showRequestDuration: true,
+    },
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();

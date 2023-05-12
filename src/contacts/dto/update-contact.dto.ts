34297d5ae8c035b03210b46da4e0c64bb4db7801
Martín <martincalderon92@outlook.com>
2023-05-12 11:15:35 -0300

    Add Update DTOs

--- src/contacts/dto/update-contact.dto.ts
+++ src/contacts/dto/update-contact.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateContactDto } from './create-contact.dto';
+import {
+  CreateDocumentDto,
+  CreatePhoneDto,
+  CreateAddressDto,
+  CreateContactDto,
+} from './create-contact.dto';
+
+export class UpdateDocumentDto extends PartialType(CreateDocumentDto) {}
+
+export class UpdatePhoneDto extends PartialType(CreatePhoneDto) {}
+
+export class UpdateAddressDto extends PartialType(CreateAddressDto) {}
 
 export class UpdateContactDto extends PartialType(CreateContactDto) {}

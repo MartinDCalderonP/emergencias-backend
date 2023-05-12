0fb5f4b6d654f4099639f46d79ecd15a48e75a4b
Martín <martincalderon92@outlook.com>
2023-05-12 12:31:42 -0300

    Add validations

--- src/contacts/dto/create-contact.dto.ts
+++ src/contacts/dto/create-contact.dto.ts
@@ -1,11 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
 
 export class CreateDocumentDto {
   id: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   type: string;
 
+  @IsNumber()
+  @IsNotEmpty()
+  @MaxLength(9)
   @ApiProperty()
   number: string;
 
@@ -15,9 +21,14 @@ export class CreateDocumentDto {
 export class CreatePhoneDto {
   id: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   type: string;
 
+  @IsNumber()
+  @IsNotEmpty()
+  @MaxLength(10)
   @ApiProperty()
   number: string;
 
@@ -27,15 +38,23 @@ export class CreatePhoneDto {
 export class CreateAddressDto {
   id: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   street: string;
 
+  @IsNumber()
+  @IsNotEmpty()
+  @MaxLength(5)
   @ApiProperty()
   number: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   city: string;
 
+  @IsString()
   @ApiProperty({ required: false })
   description: string;
 
@@ -45,18 +64,26 @@ export class CreateAddressDto {
 export class CreateContactDto {
   id: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   firstName: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   lastName: string;
 
   @ApiProperty({ type: () => CreateDocumentDto, isArray: true })
   document: CreateDocumentDto[];
 
+  @IsNumber()
+  @IsNotEmpty()
   @ApiProperty()
   age: number;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   email: string;
 
@@ -66,6 +93,8 @@ export class CreateContactDto {
   @ApiProperty({ type: () => CreateAddressDto, isArray: true })
   addresses: CreateAddressDto[];
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   documentId: string;
 }
